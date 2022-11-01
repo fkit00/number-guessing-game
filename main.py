a461f5370c3ab21d24c87dd492d06045886efa5d
Fiona <100845392+fkit00@users.noreply.github.com>
2022-11-01 09:39:58 +0000

    - made the function which takes in the number of guesses and does the logic for too high/too low

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 #Number Guessing Game Objectives:
+#from art import logo 
 
-# Include an ASCII art logo.
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
+import random
+
+#print(logo)
+
+number =30
+
+
+
+# the number guess will be random.randint(1-100)
+
+# Allow the player to submit a guess for a number between 1 and 100. do we want a function that takes in the number of guesses? 
+
+def guessing_game(number_of):
+ while number_of != 0:
+    number_of=number_of-1
+    guess = int(input('Make a guess: '))
+    if guess > number and number_of != 0:
+      print('Too high, try again')
+      print(f'You have {number_of} remaining guesses')
+    if guess < number and number_of != 0 :
+      print('Too Low, try again')
+      print(f'You have {number_of} remaining guesses')
+    if guess == number:
+      print(f"Congratulations you've guessed the number! It was {number}")
+      return
+print(f"You've run out of guesses you lose, the number was {number} ")
+      
+guessing_game(5)
+
+
+# If they run out of turns, provide feedback to the player.
+
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
